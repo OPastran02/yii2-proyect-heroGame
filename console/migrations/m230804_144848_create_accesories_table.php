6740e5fefb10c3ebd28979020a4f98151a46bf5e
OPastran02 <opastran02@gmail.com>
2023-08-04 12:14:33 -0300

    first commit

--- console/migrations/m230804_144848_create_accesories_table.php
+++ console/migrations/m230804_144848_create_accesories_table.php
@@ -28,7 +28,7 @@ class m230804_144848_create_accesories_table extends Migration
             'boost_hp' => $this->integer(8),
             'boost_sp_attack' => $this->integer(8),
             'boost_sp_defense' => $this->integer(8),
-            'boost_speedinteger(8)',
+            'boost_speed'=> $this->integer(8),,
             'boost_farming' => $this->integer(8),
             'boost_steeling' => $this->integer(8),
             'boost_wooding' => $this->integer(8),
---
+++ console/migrations/m230804_151228_create_guilds_table.php
@@ -0,0 +1,134 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%guilds}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%castle}}`
+ * - `{{%user}}`
+ * - `{{%user}}`
+ */
+class m230804_151228_create_guilds_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%guilds}}', [
+            'id' => $this->integer(11)->primaryKey(),
+            'name' => $this->string(255),
+            'description' => $this->text(),
+            'experience' => $this->integer(20),
+            'level' => $this->integer(5),
+            'wood' => $this->integer(10),
+            'steel' => $this->integer(10),
+            'food' => $this->integer(10),
+            'gold' => $this->integer(10),
+            'castle_id' => $this->string(36),
+            'quantity' => $this->integer(4),
+            'avatar' => $this->string(8),
+            'created_at' => $this->integer(11),
+            'updated_at' => $this->integer(11),
+            'created_by' => $this->string(36),
+            'updated_by' => $this->string(36),
+        ]);
+
+        // creates index for column `castle_id`
+        $this->createIndex(
+            '{{%idx-guilds-castle_id}}',
+            '{{%guilds}}',
+            'castle_id'
+        );
+
+        // add foreign key for table `{{%castle}}`
+        $this->addForeignKey(
+            '{{%fk-guilds-castle_id}}',
+            '{{%guilds}}',
+            'castle_id',
+            '{{%castle}}',
+            'id',
+            'CASCADE'
+        );
+
+        // creates index for column `created_by`
+        $this->createIndex(
+            '{{%idx-guilds-created_by}}',
+            '{{%guilds}}',
+            'created_by'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-guilds-created_by}}',
+            '{{%guilds}}',
+            'created_by',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+
+        // creates index for column `updated_by`
+        $this->createIndex(
+            '{{%idx-guilds-updated_by}}',
+            '{{%guilds}}',
+            'updated_by'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-guilds-updated_by}}',
+            '{{%guilds}}',
+            'updated_by',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%castle}}`
+        $this->dropForeignKey(
+            '{{%fk-guilds-castle_id}}',
+            '{{%guilds}}'
+        );
+
+        // drops index for column `castle_id`
+        $this->dropIndex(
+            '{{%idx-guilds-castle_id}}',
+            '{{%guilds}}'
+        );
+
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-guilds-created_by}}',
+            '{{%guilds}}'
+        );
+
+        // drops index for column `created_by`
+        $this->dropIndex(
+            '{{%idx-guilds-created_by}}',
+            '{{%guilds}}'
+        );
+
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-guilds-updated_by}}',
+            '{{%guilds}}'
+        );
+
+        // drops index for column `updated_by`
+        $this->dropIndex(
+            '{{%idx-guilds-updated_by}}',
+            '{{%guilds}}'
+        );
+
+        $this->dropTable('{{%guilds}}');
+    }
+}
