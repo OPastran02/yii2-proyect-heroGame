047836497183e209c5319e792a6bf5a6aea04804
OPastran02 <opastran02@gmail.com>
2023-08-05 11:50:23 -0300

    first commit

---
+++ api/core/shared/Domain/Bus/Command/CommandHandler.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Shared\Domain\Bus\Command;
+interface CommandHandler
+{
+}
---
+++ api/core/shared/Domain/Bus/Event/DomainEvent.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Shared\Domain\Bus\Event;
+
+use api\core\shared\Domain\Utils;
+use api\core\shared\Domain\ValueObject\UUID;
+use DateTimeImmutable;
+
+abstract class DomainEvent
+{
+    private string $eventId;
+    private string $occurredOn;
+    private string $aggregateId,
+
+    public function __construct(
+        private string $aggregateId,
+        private string $eventId = null,
+        private string $occurredOn = null
+    ) {
+        $this->aggregateId = $aggregateId;
+        $this->eventId = $eventId ?? UUID::random()->value();
+        $this->occurredOn = $occurredOn ?? Utils::dateToString(new DateTimeImmutable());
+    }
+
+    //convierte los objetos en primitivos
+    abstract public static function fromPrimitives(
+        string $aggregateId,
+        array $body,
+        string $eventId,
+        string $occurredOn
+    ): self;
+
+    abstract public function toPrimitives(): array;
+
+    abstract public static function eventName(): string;
+
+    public function aggregateId(): string
+    {
+        return $this->aggregateId;
+    }
+
+    public function eventId(): string
+    {
+        return $this->eventId;
+    }
+
+    public function occurredOn(): string
+    {
+        return $this->occurredOn;
+    }
+}
