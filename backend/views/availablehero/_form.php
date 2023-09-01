6d7d916f589bc0761334c6d73b7dc3f321a9ed79
OPastran02 <opastran02@gmail.com>
2023-09-01 18:28:42 -0300

    last usable

--- backend/views/availablehero/_form.php
+++ backend/views/availablehero/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use mihaildev\ckeditor\CKEditor;
+
 
 /** @var yii\web\View $this */
 /** @var common\models\availablehero $model */
@@ -11,106 +13,344 @@ use yii\widgets\ActiveForm;
 <div class="availablehero-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="container">
+        <div class="row">
+            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="row">
+            <?= $form->field($model, 'description')->widget(CKEditor::className(),[
+                    'editorOptions' => [
+                        'preset' => 'full',
+                        'inline' => false,
+                    ],
+                ]) 
+            ?>
+        </div>
+        <div class="row">
+            <?= $form->field($model, 'world')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="row">            
+            <?= $form->field($model, 'avatar',[
+                'template' => '
+                    <div class="custom-file">
+                        {input}
+                        {label}
+                        {error}
+                    </div>
+                ',
+                'labelOptions' => ['class' => 'custom-file-label'],
+                'inputOptions' => ['class' => 'custom-file-input'],
+            ])->textInput(['type' => 'file']) ?>
+        </div>
+        <div class="row"> 
+            <?= $form->field($model, 'avatarBlock',[
+                'template' => '
+                    <div class="custom-file">
+                        {input}
+                        {label}
+                        {error}
+                    </div>
+                ',
+                'labelOptions' => ['class' => 'custom-file-label'],
+                'inputOptions' => ['class' => 'custom-file-input'],
+            ])->textInput(['type' => 'file']) ?>        
+        </div>
+        <div class="row">  
+            <div class="col">
+                <?= $form->field($model, 'race_id')->textInput() ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'rarity_id')->textInput() ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'nature_id')->textInput() ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'type_id')->textInput() ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'attack_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'attack_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_attack_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_Battack_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'defense_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'defense_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_defense_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_defense_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'world')->textarea(['rows' => 6]) ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'hp_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'hp_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_hp_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_hp_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'avatar')->textInput(['maxlength' => true]) ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'sp_attack_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'sp_attack_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_sp_attack_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_sp_attack_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'avatarBlock')->textInput(['maxlength' => true]) ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'sp_defense_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'sp_defense_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_sp_defense_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_sp_defense_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'race_id')->textInput() ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'speed_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'speed_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_speed_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_speed_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'rarity_id')->textInput() ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'farming_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'farming_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_farming_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_farming_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'nature_id')->textInput() ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'steeling_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'steeling_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_steeling_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_steeling_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'type_id')->textInput() ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'wooding_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 2000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'wooding_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 4000
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_wooding_min')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'b_wooding_max')->textInput([
+                'maxlength' => true,
+                'type' => 'number',
+                'value' => 50
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'attack_min')->textInput() ?>
 
-    <?= $form->field($model, 'attack_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_attack_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_Battack_max')->textInput() ?>
-
-    <?= $form->field($model, 'defense_min')->textInput() ?>
-
-    <?= $form->field($model, 'defense_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_defense_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_defense_max')->textInput() ?>
-
-    <?= $form->field($model, 'hp_min')->textInput() ?>
-
-    <?= $form->field($model, 'hp_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_hp_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_hp_max')->textInput() ?>
-
-    <?= $form->field($model, 'sp_attack_min')->textInput() ?>
-
-    <?= $form->field($model, 'sp_attack_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_sp_attack_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_sp_attack_max')->textInput() ?>
-
-    <?= $form->field($model, 'sp_defense_min')->textInput() ?>
-
-    <?= $form->field($model, 'sp_defense_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_sp_defense_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_sp_defense_max')->textInput() ?>
-
-    <?= $form->field($model, 'speed_min')->textInput() ?>
-
-    <?= $form->field($model, 'speed_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_speed_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_speed_max')->textInput() ?>
-
-    <?= $form->field($model, 'farming_min')->textInput() ?>
-
-    <?= $form->field($model, 'farming_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_farming_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_farming_max')->textInput() ?>
-
-    <?= $form->field($model, 'steeling_min')->textInput() ?>
-
-    <?= $form->field($model, 'steeling_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_steeling_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_steeling_max')->textInput() ?>
-
-    <?= $form->field($model, 'wooding_min')->textInput() ?>
-
-    <?= $form->field($model, 'wooding_max')->textInput() ?>
-
-    <?= $form->field($model, 'b_wooding_min')->textInput() ?>
-
-    <?= $form->field($model, 'b_wooding_max')->textInput() ?>
-
-    <?= $form->field($model, 'available')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'updated_by')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'available')->checkbox() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
